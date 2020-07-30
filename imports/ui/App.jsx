807b8e4282c8671bd34cbf2beead9ee11ce77340
nsoto92 <nor.soto92@gmail.com>
2020-07-29 19:38:43 -0700

    filtering works

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';  // hook used to render tasks
+import _ from 'lodash'
 import { Task } from './Task'
 import Tasks from '../api/tasks'
 import { TaskForm } from './TaskForm'
@@ -15,11 +16,31 @@ const toggleChecked = ({ _id, isChecked }) => {  //Toggle checkbox
 const deleteTask = ({ _id }) => Tasks.remove(_id);
 
 export const App = () => {
-  const tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());  //Shows newest tasks first
+  const filter = {};
+
+  const [hideCompleted, setHideCompleted] = useState(false);
+
+  if (hideCompleted) {
+    _.set(filter, 'checked', false);
+  }
+
+  const tasks = useTracker(() => Tasks.find(filter, { sort: { createdAt: -1 } }).fetch());  //Shows newest tasks first and filters them
 
   return (
     <div className="simple-todos-react">
       <h1>Norbert's Task List</h1>
+      {/* FIltering Checkbox */}
+      <div className="filters">
+        <label>
+          <input
+            type="checkbox"
+            readOnly
+            checked={Boolean(hideCompleted)}
+            onClick={() => setHideCompleted(!hideCompleted)}
+          />
+          Hide Completed
+        </label>
+      </div>
       {/* Renders list items*/}
       <ul className="tasks">
         {tasks.map(task => <Task
