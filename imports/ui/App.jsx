bc9d8bd4e49101bb23b9db9b93727b21cea3f391
nsoto92 <nor.soto92@gmail.com>
2020-07-29 19:20:30 -0700

    items posting and deleting

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,15 +4,30 @@ import { Task } from './Task'
 import Tasks from '../api/tasks'
 import { TaskForm } from './TaskForm'
 
+const toggleChecked = ({ _id, isChecked }) => {  //Toggle checkbox
+  Tasks.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => Tasks.remove(_id);
+
 export const App = () => {
-  const tasks = useTracker(() => Tasks.find({}).fetch());  //
+  const tasks = useTracker(() => Tasks.find({}, { sort: { createdAt: -1 } }).fetch());  //Shows newest tasks first
 
   return (
     <div>
-      <h1>Welcome to Meteor!</h1>
+      <h1>Norbert's Task List</h1>
       {/* Renders list items*/}
       <ul>
-        {tasks.map(task => <Task key={task._id} task={task} />)}
+        {tasks.map(task => <Task
+          key={task._id}
+          task={task}
+          onCheckboxClick={toggleChecked}
+          onDeleteClick={deleteTask}
+        />)}
       </ul>
 
       <TaskForm />
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 //Defines tasks
-export const Task = ({ task }) => {
-    return <li>{task.text}</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+    return (
+        <li>
+            <button onClick={() => onDeleteClick(task)}>&times;</button>
+            <input
+                type="checkbox"
+                checked={Boolean(task.isChecked)}
+                onClick={() => onCheckboxClick(task)}
+                readOnly
+            />
+            <span>{task.text}</span>
+        </li>
+    );
 };
